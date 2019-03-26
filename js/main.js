b55b3356f711170e94509e60c15398d5568ae62c
Rauno Kaldmaa <rauno.tabularasa@gmail.com>
2019-03-26 22:08:53 +0200

    services section
    removed owl carousel and implemented grid system

--- js/main.js
+++ js/main.js
@@ -91,15 +91,15 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
+  // $('.services-carousel').owlCarousel({
+  //     autoplay: true,
+  //     loop: true,
+  //     margin: 20,
+  //     dots: true,
+  //     nav: false,
+  //     responsiveClass: true,
+  //     responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+  //   });
 
   // ========================================================================= //
   //  magnificPopup
